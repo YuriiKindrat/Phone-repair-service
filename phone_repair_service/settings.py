460bf60cd5878903103881befd7b88a7516d544e
Yurii Kindrat <yurkinkek@gmail.com>
2022-08-07 18:47:59 +0300

    created the main logic

--- phone_repair_service/settings.py
+++ phone_repair_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,6 +124,18 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 PHONENUMBER_DEFAULT_REGION = "UA"
 
 AUTH_USER_MODEL = 'user.CustomUser'
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=100),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
+    "ROTATE_REFRESH_TOKENS": False,
+}
--- repair_service/models.py
+++ repair_service/models.py
@@ -5,24 +5,25 @@ from django.db import models
 class Request(models.Model):
 
     STATUS_CHOICES = [
-        ("R", "Ready"),
-        ("W", "In work"),
-        ("L", "In line"),
+        ("Ready", "Ready"),
+        ("In work", "In work"),
+        ("In line", "In line"),
     ]
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    phone_model = models.CharField(max_length=255)
-    problem_description = models.TextField()
-    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="L")
+    phone_model = models.CharField(max_length=255, null=True)
+    problem_description = models.TextField(null=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="In line")
 
     def __str__(self):
-        return f"{self.id}, ({self.user}, {self.status})"
+        return f"Request id: {self.id}, ({self.user}, {self.status})"
 
 
 class Invoice(models.Model):
 
     request = models.ForeignKey("Request", on_delete=models.SET_NULL, null=True)
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+    paid = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.request}, {self.price}"
